f7f410567f35fc777985aeb0e1f04e02a7282e7e
Chelsea Chen <chelseachen893@gmail.com>
2020-08-07 18:20:41 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Chelsea");
+
+        int myFirstNumber = (5 + 10) + (2*10);
+        System.out.println(myFirstNumber);
+        int mySecondNumber = 12;
+        System.out.println(mySecondNumber);
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = mySecondNumber + myThirdNumber + myFirstNumber;
+        System.out.println(myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+
+    }
+}
